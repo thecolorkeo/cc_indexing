7820480c7b961459af1f2f0fb0998cd71124f077
Keo Chan <thecolorkeo@gmail.com>
2019-03-14 21:55:39 -0400

    update comments

--- src/build_dict.py
+++ src/build_dict.py
@@ -10,21 +10,36 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit
 
+'''
+Distributed solution for Insight's indexing
+coding challenge using Apache Spark. Reads in
+a list of documents and constructs an
+inverted index.
+'''
+
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 spark = SparkSession.builder.getOrCreate()
 
+def main():
+	try:
+		input_path = sys.argv[1]
+		output_path = sys.argv[2]
+	except:
+		print("Invalid input: provide input path then output path")
+	build_dict(input_path, output_path)
+
 def build_dict(input_path, output_path):
 	'''
-	Reads documents in path folder
+	Reads documents in input path dir
 	and collects every unique pair of
 	(wordID, docID)
 	'''
 
 	files = os.listdir(input_path)
-	rdds = [] # list of indices per document
+	rdds = [] # list of word indices per document
 
-
+	# Read in files individually, parse text and create dictionary
 	for filename in files:
 		words_seen_in_file = {}
 		f = spark.read.text(input_path + '/' + filename) \
@@ -37,17 +52,14 @@ def build_dict(input_path, output_path):
 			.distinct()
 		rdds.append(f)
 
+	# Combine (and invert) array of dictionaries from each document into one index
 	output =  sc.union(rdds) \
 				.keyBy(lambda x: x[1]) \
 				.mapValues(lambda x: x[0]) \
 				.groupByKey() \
 				.map(lambda x: (x[0], list(x[1])))
-	output.coalesce(1).saveAsTextFile(output_path)
 
-def main():
-	input_path = sys.argv[1]
-	output_path = sys.argv[2]
-	build_dict(input_path, output_path)
+	output.coalesce(1).saveAsTextFile(output_path)
 
 if __name__ == "__main__":
 	main()
